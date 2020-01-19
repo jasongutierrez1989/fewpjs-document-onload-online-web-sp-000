e16a7e1c299cd590f321f5d86d01b2d3bfa9932c
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-01-19 05:08:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-  document.querySelector('text').innerHTML = "This is really cool!";
+  document.querySelector('p').innerHTML = "This is really cool!";
 });
